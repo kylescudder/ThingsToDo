846f61e70b53ec1678a81d0f630d898d754d806f
Kyle Scudder <kyle.s@paloma.co.uk>
2022-04-22 16:50:52 +0100

    Moved DarkModeToggle function to component onMount

--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -1,3 +1,45 @@
+<script lang="ts">
+import { onMount } from "svelte";
+
+	onMount(() => {
+		var themeToggleBtn = document.querySelector('.toggle--label') as HTMLLabelElement;
+		var themeToggleCheckbox = document.querySelector('.toggle--checkbox') as HTMLInputElement;
+
+		if (themeToggleBtn !== null) {
+			themeToggleBtn.addEventListener('click', function() {
+				// if set via local storage previously
+				if (localStorage.getItem('color-theme')) {
+					if (localStorage.getItem('color-theme') === 'light' || !('color-theme' in localStorage)) {
+						setDarkTheme();
+					} else {
+						setLightTheme();
+					}
+				} else {
+					if (document.documentElement.classList.contains('dark')) {
+						document.documentElement.classList.remove('dark');
+						localStorage.setItem('color-theme', 'light');
+					} else {
+						document.documentElement.classList.add('dark');
+						localStorage.setItem('color-theme', 'dark');
+					}
+				}
+			});
+		}
+		// Change the icons inside the button based on previous settings
+		if (localStorage.getItem('color-theme') === 'dark') {
+				setDarkTheme()
+				themeToggleCheckbox.checked = true
+		}
+		function setLightTheme() {
+			document.documentElement.classList.remove('dark');
+			localStorage.setItem('color-theme','light');
+		}
+		function setDarkTheme() {
+			document.documentElement.classList.add('dark');
+			localStorage.setItem('color-theme','dark');
+		}
+	})
+</script>
 <input type="checkbox" id="toggle" class="toggle--checkbox" />
 <label for="toggle" class="toggle--label float-right mr-2">
 	<span class="toggle--label-background" />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,44 +15,6 @@ import { onMount } from 'svelte';
 	let checklistLogo = './src/images/checklist.png'
 	export let userId: number
 
-	onMount(() => {
-		var themeToggleBtn = document.querySelector('.toggle--label') as HTMLLabelElement;
-		var themeToggleCheckbox = document.querySelector('.toggle--checkbox') as HTMLInputElement;
-
-		if (themeToggleBtn !== null) {
-			themeToggleBtn.addEventListener('click', function() {
-				// if set via local storage previously
-				if (localStorage.getItem('color-theme')) {
-					if (localStorage.getItem('color-theme') === 'light' || !('color-theme' in localStorage)) {
-						setDarkTheme();
-					} else {
-						setLightTheme();
-					}
-				} else {
-					if (document.documentElement.classList.contains('dark')) {
-						document.documentElement.classList.remove('dark');
-						localStorage.setItem('color-theme', 'light');
-					} else {
-						document.documentElement.classList.add('dark');
-						localStorage.setItem('color-theme', 'dark');
-					}
-				}
-			});
-		}
-		// Change the icons inside the button based on previous settings
-		if (localStorage.getItem('color-theme') === 'dark') {
-				setDarkTheme()
-				themeToggleCheckbox.checked = true
-		}
-		function setLightTheme() {
-			document.documentElement.classList.remove('dark');
-			localStorage.setItem('color-theme','light');
-		}
-		function setDarkTheme() {
-			document.documentElement.classList.add('dark');
-			localStorage.setItem('color-theme','dark');
-		}
-	});
 </script>
 
 <svelte:head>
