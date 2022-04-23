856fd6741ec29a2cdf7fc610bb9c2ef8c7560dd6
kylescudder <74150974+kylescudder@users.noreply.github.com>
2022-04-23 18:11:23 +0100

    Notify package added for success update toast

---
+++ src/components/ToDo.svelte
@@ -0,0 +1,19 @@
+	async function successToast() {
+		new Notify ({
+			status: 'success',
+			title: 'Notify Title',
+			text: 'Notify text lorem ipsum',
+			effect: 'slide',
+			speed: 300,
+			customClass: '',
+			customIcon: '',
+			showIcon: true,
+			showCloseButton: true,
+			autoclose: true,
+			autotimeout: 3000,
+			gap: 20,
+			distance: 20,
+			type: 1,
+			position: 'right bottom'
+		})
+	}
