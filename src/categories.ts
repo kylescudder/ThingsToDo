2977765513d2723f7250aa16c3c8faae03f62689
kylescudder <74150974+kylescudder@users.noreply.github.com>
2022-04-27 17:35:19 +0100

    Add Category functionality added

--- src/categories.ts
+++ src/categories.ts
@@ -1,3 +1,9 @@
+import type { todo } from "./interfaces";
+import { getToDo } from "./todo";
+
+export let categoryText: string
+export let todos: Array<todo> = [];
+
 export async function categoriesPopulate(apiBaseUrl: string, userId: number) {
 	const categoriesResponse = await fetch(`${apiBaseUrl}/categories`, {
 		method: "POST",
@@ -11,6 +17,30 @@ export async function categoriesPopulate(apiBaseUrl: string, userId: number) {
 	const categoriesPayload = await categoriesResponse.json();
 	return categoriesPayload.payload;
 }
-export async function addCategory(newCategory: string) {
-	alert(newCategory)
+export async function addCategory(newCategory: string, apiBaseUrl: string, userId: number) {
+	todos = []
+	const response = await fetch(`${apiBaseUrl}/category`, {
+		method: 'POST',
+		body: JSON.stringify({
+			categoryText: newCategory,
+			githubId: userId,
+		}),
+		headers: {
+			'content-type': 'application/json',
+		},
+	})
+	let categoryId: string
+	if (response.status === 200) {
+		const payload = await response.json();
+		categoryId = payload.data.categoryId
+		categoriesPopulate(apiBaseUrl, userId)    
+			.catch(() => {
+				console.log('Getting categories failed')
+			})
+		todos = await getToDo(categoryId, apiBaseUrl, userId)
+		return todos
+	} else {
+		alert('Failed to add category')
+		return todos
+	}
 }
--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
+	import type { todo } from 'src/interfaces';
+	import { addCategory } from '../categories'
+	
 	export let id: string
 	export let title: string
-	export let newCategory: string
-	export let clickFuncion
+	export let apiBaseUrl: string
+	export let userId: number
+	export let todos: Array<todo> = []
+	export let categoryText: string
+	let newCategory: string
+
+	async function categoryAdd() {
+		const btnCloseModal = document.querySelector('.btnCloseModal') as HTMLButtonElement
+		if (btnCloseModal !== null) {
+			btnCloseModal.click()
+		}
+		categoryText = newCategory
+	 	todos = await addCategory(newCategory, apiBaseUrl, userId)
+	}
 </script>
 <!-- Modal -->
 <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
@@ -20,48 +35,11 @@
 				</button>
       </div>
       <div class="modal-body relative p-4">
-	      <input
-					type="text"
-					bind:value={newCategory}
-					class="
-						form-control
-						block
-						w-full
-						px-3
-						py-1.5
-						text-base
-						font-normal
-						text-gray-700 dark:text-white
-						bg-white dark:bg-gray-500 bg-clip-padding
-						border border-solid border-gray-300
-						rounded
-						transition
-						ease-in-out
-						m-0
-						focus:text-gray-500 focus:bg-white focus:border-blue-600 focus:outline-none
-					"
-					id="exampleFormControlInput1"
-					placeholder="Example label"
-				/>
+	      <input type="text" bind:value={newCategory} class="form-control"
+					id="exampleFormControlInput1" placeholder="Example label" />
       </div>
       <div class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 rounded-b-md">
-				<button type="button" on:click={clickFuncion()} class="px-6
-					py-2.5
-					bg-blue-600
-					text-white
-					font-medium
-					text-xs
-					leading-tight
-					uppercase
-					rounded
-					shadow-md
-					hover:bg-blue-700 hover:shadow-lg
-					focus:bg-blue-700 focus:shadow-lg 
-					focus:outline-none focus:ring-0
-					active:bg-blue-800 active:shadow-lg
-					transition
-					duration-150
-					ease-in-out">Add</button>
+				<button type="button" on:click={() => categoryAdd()} class="btnAdd">Add</button>
 				<button type="button" class="btnCloseModal" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
