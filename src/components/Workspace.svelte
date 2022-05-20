43c2b34e44f17d8143247d0fab46212e247a68c6
kylescudder <74150974+kylescudder@users.noreply.github.com>
2022-05-20 14:48:29 +0100

    More work on iOS min-height

--- src/components/Workspace.svelte
+++ src/components/Workspace.svelte
@@ -27,7 +27,7 @@
 {#if isLoading}
 	<Loading />
 {:else}
-	<div class="relative min-h-screen md:flex" data-dev-hint="container">		
+	<div class="relative md:flex h-screen mobileContainer" data-dev-hint="container">		
 		<Modal
 			show={$modal}
 			unstyled={true}
