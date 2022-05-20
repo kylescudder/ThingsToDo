ca7ec2c292a529a6398e6990cdff9b7fe8dbe80e
kylescudder <74150974+kylescudder@users.noreply.github.com>
2022-05-20 14:31:41 +0100

    Added flex to logout button icon

--- src/components/LogoutButton.svelte
+++ src/components/LogoutButton.svelte
@@ -1,8 +1,8 @@
 <script>
 	import FaSignOutAlt from 'svelte-icons/fa/FaSignOutAlt.svelte'
 </script>
-<a href="/logout" class="absolute bottom-5 text-white no-underline float-right ml-3">
-	<div class="text-white w-6 inline-table">
+<a href="/logout" class="absolute flex justify-center bottom-5 text-white no-underline float-right ml-3">
+	<div class="text-white w-6 inline-table mr-3">
 		<FaSignOutAlt />
 	</div>
 	Logout
