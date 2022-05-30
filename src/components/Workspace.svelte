d2d2ccaf535be3398233104e6d3e48ec8d664eb6
Kyle Scudder <kyle.s@paloma.co.uk>
2022-05-30 16:59:54 +0100

    Added mobileContainer for side bar

--- src/components/Workspace.svelte
+++ src/components/Workspace.svelte
@@ -27,7 +27,7 @@
 {#if isLoading}
 	<Loading />
 {:else}
-	<div class="relative md:flex" data-dev-hint="container">		
+	<div class="relative mobileContainer md:flex" data-dev-hint="container">		
 		<Modal
 			show={$modal}
 			unstyled={true}
